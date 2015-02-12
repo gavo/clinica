9d2061b4684aa020b86a0cebd3c782b632886e9e
Gustavo Vargas Miranda <gvmgust@gmail.com>
2015-02-12 17:57:55 -0400

    Creando clase abstracta SQL que permitira:
    	fasilitar agrupar todas las consultas sql en un solo lugar
    	desplegar un metodo de realizar pregunta de confirmacion para cuando sea necesario
    	encriptar texto en SHA1 a travez de el metodo SHA1 creado
    	Convertir fechas al formato de fechas de SQL (con horas, sin horas)

---
+++ implementacion/Clinica/src/utils/SQL.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author CKN
+ */
+public abstract class SQL {
+    /////////////////////HERRAMIENTAS USADAS PARA LAS CONSULTAS/////////////////////
+
+    public static String sha1(String string) {
+        String hash = "";
+        try {
+            MessageDigest md;
+            byte[] buffer, digest;
+            buffer = string.getBytes();
+            md = MessageDigest.getInstance("SHA1");
+            md.update(buffer);
+            digest = md.digest();
+            for (byte aux : digest) {
+                int b = aux & 0xff;
+                if (Integer.toHexString(b).length() == 1) {
+                    hash += "0";
+                }
+                hash += Integer.toHexString(b);
+            }
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(SQL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return hash;
+    }
+    public static final SimpleDateFormat formatoDateTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    public static final SimpleDateFormat formatDates = new SimpleDateFormat("yyyy-MM-dd");
+
+    public static boolean pregunta(String pregunta) {
+        int seleccion = JOptionPane.showOptionDialog(
+                null, pregunta,
+                "Seleccione una opción", // Título
+                JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null, // null para icono por defecto.
+                new Object[]{"Si", "No"}, // null para YES, NO y CANCEL
+                "Si");
+        if (seleccion != -1) {
+            if ((seleccion + 1) == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+// REGISTRO DE SQL EJECUTADO EN LA DB
+    public static String backup(String sql) {
+        return "INSERT INTO `backup`(`sql`)VALUES(\"" + sql + "\");";
+    }
+}
