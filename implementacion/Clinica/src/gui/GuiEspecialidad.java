d4a2258d794147ffbc3aac0edddcd1a098106d06
Gustavo Vargas Miranda <gvmgust@gmail.com>
2015-02-18 12:18:37 -0400

    Modificando GuiEspecialidad para que permita unicamente modificar y agregar especialidades

--- implementacion/Clinica/src/gui/GuiEspecialidad.java
+++ implementacion/Clinica/src/gui/GuiEspecialidad.java
@@ -7,61 +7,67 @@ package gui;
 
 import data.Especialidad;
 import domains.ManagerEspecialidad;
-import java.util.ArrayList;
 
 /**
  *
  * @author CKN
  */
 public class GuiEspecialidad extends javax.swing.JFrame {
-
+    
     private static GuiEspecialidad myInstance;
-    private Especialidad puntero;
-    public ArrayList<Especialidad> lista;
-
+    private Especialidad e;
+    
     private GuiEspecialidad() {
         initComponents();
         this.setVisible(true);
         this.setLocationRelativeTo(null);
-        this.setTitle("Especialidad");
-        cargarLista();
-        puntero = null;
+        this.setTitle("Agregando Especialidad");
     }
-
+    
+    private GuiEspecialidad(Especialidad e) {
+        initComponents();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        this.setTitle("Modificando Especialidad " + e.getNombre());
+        this.e = e;
+        jTextField1.setText(e.getNombre());
+        jTextField2.setText(e.getCosto()+"");
+    }
+    
     public static GuiEspecialidad get() {
         if (myInstance == null) {
             myInstance = new GuiEspecialidad();
         }
         return myInstance;
     }
-
-    private void cargarLista() {
-        lista = ManagerEspecialidad.listarEspecialidad();
-        jComboBox1.removeAllItems();
-        for (Especialidad lista1 : lista) {
-            jComboBox1.addItem(lista1.getNombre());
+    
+    public static GuiEspecialidad get(Especialidad e) {
+        if (myInstance == null) {
+            myInstance = new GuiEspecialidad(e);
         }
+        return myInstance;
     }
-
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jComboBox1 = new javax.swing.JComboBox();
         jTextField1 = new javax.swing.JTextField();
         jTextField2 = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jButton2 = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setResizable(false);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosing(java.awt.event.WindowEvent evt) {
+                formWindowClosing(evt);
+            }
+        });
 
         jPanel1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 1, true));
-
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
         jButton1.setText("Guardar");
         jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
@@ -75,36 +81,22 @@ public class GuiEspecialidad extends javax.swing.JFrame {
 
         jLabel2.setText("Precio:");
 
-        jButton2.setText("Modificar");
-        jButton2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
-            }
-        });
-
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jTextField1)
+                    .addComponent(jTextField2)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 186, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton2))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1)
-                            .addComponent(jLabel2))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jTextField1)
-                            .addComponent(jTextField2)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 163, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(0, 0, Short.MAX_VALUE)))))
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 163, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 50, Short.MAX_VALUE)))
                 .addContainerGap(14, Short.MAX_VALUE))
         );
 
@@ -113,11 +105,7 @@ public class GuiEspecialidad extends javax.swing.JFrame {
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton2))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(16, 16, 16)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -127,7 +115,7 @@ public class GuiEspecialidad extends javax.swing.JFrame {
                     .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                         .addComponent(jLabel2)
                         .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 11, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jButton1)
                 .addContainerGap())
         );
@@ -152,32 +140,31 @@ public class GuiEspecialidad extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        puntero = lista.get(jComboBox1.getSelectedIndex());
-        jTextField1.setText(puntero.getNombre());
-        jTextField2.setText(puntero.getCosto() + "");
-    }//GEN-LAST:event_jButton2ActionPerformed
-
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        if (puntero == null) {
-            puntero = new Especialidad(jTextField1.getText(),Float.parseFloat(jTextField2.getText()));
-            ManagerEspecialidad.registrarEspecialidad(puntero);
-        }else{
-            puntero.setNombre(jTextField1.getText());
-            puntero.setCosto(Float.parseFloat(jTextField2.getText()));
-            ManagerEspecialidad.actualizarEspecialidad(puntero);
+        boolean isNull = false;
+        if (e == null) {
+            isNull = true;
+            e = new Especialidad();
         }
-        puntero = null;
-        jTextField1.setText("");
-        jTextField2.setText("");
-        cargarLista();
+        e.setNombre(jTextField1.getText());
+        e.setCosto(Float.parseFloat(jTextField2.getText()));
+        if (isNull) {
+            ManagerEspecialidad.registrarEspecialidad(e);
+        } else {
+            ManagerEspecialidad.actualizarEspecialidad(e);
+        }
+        GuiAdminEspUs.CargarLista();
+        myInstance = null;
+        this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void formWindowClosing(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosing
+        myInstance = null;
+    }//GEN-LAST:event_formWindowClosing
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JComboBox jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
